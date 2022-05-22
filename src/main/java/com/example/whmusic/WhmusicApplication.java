2554330092169b90571931f226686b43219b9378
W H <wh@example.com>
2022-05-22 23:03:35 +0800

    first commit

---
+++ src/main/java/com/example/whmusic/WhmusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.whmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhmusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhmusicApplication.class, args);
+    }
+
+}
